d8ad84d16054b67a9ab506914eac1acf9d06a9bd
Peer Reynders <17050883+peerreynders@users.noreply.github.com>
2023-12-23 23:42:30 -0500

    Restart: beta 2 conversion

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,40 +1,20 @@
-import {
-	createHandler,
-	renderAsync,
-	StartServer,
-	type MiddlewareInput,
-	type MiddlewareFn,
-} from 'solid-start/entry-server';
-import { redirect } from 'solid-start/server';
-import { getUser, logout } from './server/session';
-import { homeHref, loginHref, logoutHref } from './route-path';
+import { createHandler, StartServer } from "@solidjs/start/server";
 
-// --- BEGIN dev dependency
-import { start as startRepo } from '~/server/repo';
-
-startRepo();
-// --- END dev dependency
-
-const protectedPaths = new Set([homeHref]);
-
-function userMiddleware({ forward }: MiddlewareInput): MiddlewareFn {
-	return async (event) => {
-		const loginRoute = loginHref();
-		const route = new URL(event.request.url).pathname;
-		if (route === logoutHref) return logout(event.request, loginRoute);
-
-		// Attach user to FetchEvent if available
-		const user = await getUser(event.request);
-
-		if (!user && protectedPaths.has(route)) return redirect(loginHref(route));
-
-		event.locals['user'] = user;
-
-		return forward(event);
-	};
-}
-
-export default createHandler(
-	userMiddleware,
-	renderAsync((event) => <StartServer event={event} />)
-);
+export default createHandler(() => (
+  <StartServer
+    document={({ assets, children, scripts }) => (
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+          {assets}
+        </head>
+        <body>
+          <div id="app">{children}</div>
+          {scripts}
+        </body>
+      </html>
+    )}
+  />
+));
